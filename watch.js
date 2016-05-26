712e852009f5fa8d6a8da7fa95ec883dabcda95b
Dan Dietz <danddietz@gmail.com>
2016-05-26 12:48:45 -0400

    Clock counting

--- watch.js
+++ watch.js
@@ -2,11 +2,57 @@
   'use strict';
 
   var startButton = document.getElementById('start-button');
+  var hours = document.querySelector('.hours');
+  var minutes = document.querySelector('.minutes');
+  var seconds = document.querySelector('.seconds');
+
+  var startTime;
+  var currentText;
 
   console.log(startButton);
 
+  function toggleButton(){
+    currentText = startButton.textContent;
+
+    if(currentText == 'Start'){
+      startButton.textContent = 'Stop';
+      startButton.style.backgroundColor = 'red';
+    } else{
+      startButton.textContent = 'Start';
+      startButton.style.backgroundColor = 'green';
+    }
+  }
+
+  function millisecondsToInterval(ms){
+    var numMsInHour = 60000 * 60;
+    var numMsInMinute = 60000;
+    var numMsInSecond = 1000;
+
+    var hourCount = Math.floor(ms / numMsInHour);
+    var minuteCount = Math.floor(ms / numMsInMinute);
+    var secondCount = Math.floor(ms / numMsInSecond);
+
+    return [hourCount, minuteCount, secondCount];
+  }
+
+  function displayInterval(interval){
+    hours.textContent = ("0" + interval[0]).slice(-2);
+    minutes.textContent = ("0" + interval[1]).slice(-2);
+    seconds.textContent = ("0" + interval[2]).slice(-2);
+  }
+
+  function logElapsedTime(){
+    var currentTime = new Date();
+    var elapsedTime = currentTime - startTime;
+    var elasped = millisecondsToInterval(elapsedTime);
+
+    displayInterval(elasped);
+  }
+
   function startButtonHandler(){
-    alert('you clicked on me!');
+    startTime = new Date();
+    toggleButton();
+    window.setInterval(logElapsedTime, 1000);
   }
 
   startButton.addEventListener('click', startButtonHandler);
